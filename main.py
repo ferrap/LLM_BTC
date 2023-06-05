e020f9f7ad2d80fe6a5126e3d2346f9c4f8aa145
Paolo Ferraiuoli <48998427+ferrap@users.noreply.github.com>
2023-06-05 10:55:55 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import requests
+
+df = pd.DataFrame()
+URL = 'https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=USD&allData=true' 
+res = requests.get(URL)
+res_json = res.json()
+data= res_json['Data']
+temp_df= pd.DataFrame(data)
+temp_df['conversionSymbol']= 'USD'
+df = df.append(temp_df, ignore_index=True)
+df['time']=pd.to_datetime(df['time'],unit='s')
+df['time'] = df['time'].dt.strftime('%d/%m/%Y')
