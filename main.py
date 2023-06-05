a33bf4ce91325f22b0103b8903b0502876c08bb9
Paolo Ferraiuoli <48998427+ferrap@users.noreply.github.com>
2023-06-05 11:40:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import os 
+from langchain.llms import OpenAI
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.indexes import VectorstoreIndexCreator
+import streamlit as st
+from streamlit_chat import message
 import pandas as pd
-import requests
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.chat_models import ChatOpenAI
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.DataFrame()
 URL = 'https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=USD&allData=true' 
@@ -12,7 +22,7 @@ df = pd.concat([df,temp_df], ignore_index=True)
 df['time']=pd.to_datetime(df['time'],unit='s')
 df['time'] = df['time'].dt.strftime('%d/%m/%Y')
 
-"""Python file to serve as the frontend"""
+
 import streamlit as st
 from streamlit_chat import message
 
@@ -20,3 +30,23 @@ from langchain.chains import ConversationChain
 from langchain.llms import OpenAI
 
 open_ai_key = st.secrets["OPENAI_API_KEY"]
+
+llm = ChatOpenAI(temperature=0)
+agent = create_pandas_dataframe_agent(llm, df, verbose=True)
+
+# Setup streamlit app
+# Display the page title and the text box for the user to ask the question
+st.title('✨ Query your Data ')
+prompt = st.text_input("Enter your question to query your PDF documents")
+
+
+if prompt:
+    # Get the resonse from LLM
+    # We pass the model name (3.5) and the temperature (Closer to 1 means creative resonse)
+    # stuff chain type sends all the relevant text chunks from the document to LLM
+
+    response =  agent.run(prompt)
+
+
+    # Write the results from the LLM to the UI
+    st.write("<b>" + prompt + "</b><br><i>" + response + "</i><hr>", unsafe_allow_html=True )
